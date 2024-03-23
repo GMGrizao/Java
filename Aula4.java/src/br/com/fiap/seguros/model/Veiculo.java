f5b13a266bc19a3a551e2bf7929d2e2b2a795609
GMGrizao <guilhermemenezes2102@gmail.com>
2024-03-22 22:48:58 -0300

    Aula04

---
+++ Aula4.java/src/br/com/fiap/seguros/model/Veiculo.java
@@ -0,0 +1,2 @@
+package br.com.fiap.seguros.model;public class Veiculo {
+}
---
+++ Aula4.java/src/br/com/fiap/seguros/view/Main.java
@@ -0,0 +1,12 @@
+package br.com.fiap.seguros.view;
+
+import br.com.fiap.seguros.model.Cliente;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //I
+
+
+    }
+}
